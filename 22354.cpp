50c28c65a587ec9e071e266009f87cbaab919068
seungy0 <51262434+seungy0@users.noreply.github.com>
2022-06-24 21:39:07 +0900

    BOJ 22354 solved (플레티넘 문젠데 막상 풀어보니 쉬웠음)

---
+++ 22354.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> nodes;
+int n;
+void input(){
+    cin >> n;
+    string wb;
+    cin >> wb;
+    char prev;
+    int m,tmp;
+    cin >> m;
+    for(int i=1; i<n ;i++){
+        cin >> tmp;
+        if(wb[i]!=wb[i-1]){
+            nodes.push_back(m);
+            m=tmp;
+        }
+        else if(m<tmp){
+            m=tmp;
+        }
+    }
+    nodes.push_back(m);
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    input();
+    nodes[0]=0;
+    nodes[nodes.size()-1]=0;
+    sort(nodes.begin(), nodes.end());
+    long long ans=0;
+    int cnt = (int)ceil((nodes.size()-2)/2.0);
+    for(int i=nodes.size()-1; i>=nodes.size()-cnt; i--){
+        ans+=(long long)nodes[i];
+    }
+    cout << cnt;
+    cout << ans;
+}
